e20199cf2b6f4d2e9dfe94d8cb91b2af5a1d11ef
Future <72302164+camelCasing@users.noreply.github.com>
2023-02-18 21:47:54 +0000

    Create code.cpp

---
+++ c++/code.cpp
@@ -0,0 +1,25 @@
+#include "common.h"
+
+int doubleInt(int first) {
+    return first * 2;
+}
+
+int main() {
+    // Credit to The Cherno for epic series!
+
+    int variable = 5;
+
+    if (variable == 1) {
+        std::cout << "variable is 1" << std::endl;
+    } else {
+        std::cout << "variable is not 1" << std::endl;
+    }
+
+    std::cout << doubleInt(5) << std::endl;
+
+    std::cout << variable << std::endl;
+
+    for (int i = 0; i < variable; i++) {
+        std::cout << i << std::endl;
+    }
+}
